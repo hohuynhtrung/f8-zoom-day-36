f143239958312e66328bbd3d2ff148dd74051f88
HoHuynhTrung <hohuynhtrung2003@gmail.com>
2026-06-25 17:00:02 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Router, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router basename="/f8-zoom-day-36/">
+      <main className="body-container">
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/f8-zoom-day-36",
+});
